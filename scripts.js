d4097b063b5b34cce0262bf8c58d1411ed465e59
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-06-12 11:26:25 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Desh Raj");
+$("#policyNumber").text("DC01072824");
+$("#startDate").text("13-Jun-2020");
+$("#endDate").text("12-Jun-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB08BS0277");
+$("#make").text("MARUTI");
+$("#model").text("ALTO K10");
+$("#vehicalSubType").text("LXI");
+$("#engineNo").text("155255");
+$("#chassis").text("592565");
